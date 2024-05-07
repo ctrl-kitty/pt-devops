11574754d1b26bd7af6f17222e4822aa6b1d9a1a
Your Name <you@example.com>
2024-05-07 10:50:33 +0000

    postgres fixes+init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,12 @@
+-- Создание таблицы для электронной почты
+CREATE TABLE email (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE
+);
+
+-- Создание таблицы для номеров телефонов
+CREATE TABLE phone (
+    id SERIAL PRIMARY KEY,
+    phone_number VARCHAR(20) UNIQUE
+);
 
